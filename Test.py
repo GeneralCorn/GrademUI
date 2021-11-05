2aa95bedb231f2c93c0c877fb8c066df7e183012
Uzen Huang <uzen.huang@gmail.com>
2021-11-05 22:56:34 +0800

    changed fs

--- Test.py
+++ Test.py
@@ -1,33 +1,17 @@
-# +
+#!/usr/bin/env python3
 from csv import reader
+
+fs = list()
 
 with open('./sentences.csv', 'r') as read_obj:
 
     csv_reader = reader(read_obj)
     
-    fs7  = []
-    fs6 = []
+    for i in range(7):
+        fs.append(list())
 
     for col in csv_reader:
-        fs7.append(col[0])
-        fs6.append(col[1])
+        for i in range(7):
+            fs[i].append(col[0])
 
-
-    list_of_rows = []
-    list_of_rows.append(fs7)
-    list_of_rows.append(fs6)
-    print(list_of_rows)
-# -
-
-from csv import reader
-# read csv file as a list of lists
-with open('./sentences.csv', 'r') as read_obj:
-    # pass the file object to reader() to get the reader object
-    csv_reader = reader(read_obj)
-    # Pass reader object to list() to get a list of lists
-    list_of_rows = list(csv_reader)
-    print(list_of_rows)
-
-
-
-
+print(fs) # fs = list_of_rows
