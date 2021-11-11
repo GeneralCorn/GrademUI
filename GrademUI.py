74ba2ec8512606f5807751dac2d849dec2bdff48
Cobbi <cobbi2004@gmail.com>
2021-11-11 11:13:19 +0800

    Excel Support

--- GrademUI.py
+++ GrademUI.py
@@ -54,7 +54,7 @@ if sentences is not None:
         for i in commentbank:
             del i[0]
     else:
-        df1 = pd.read_excel(sentences)
+        df1 = pd.read_excel(open(sentences, 'r'))
         commentbank = df1.values.tolist()
         for i in commentbank:
             del i[0]
@@ -66,7 +66,7 @@ if stu is not None:
         df = pd.read_csv(stu)
         studentinfo = df.values.tolist()
     else:        
-        df = pd.read_excel(stu)
+        df = pd.read_excel(open(stu),'r')
         studentinfo = df.values.tolist()
 else:
     st.stop()
